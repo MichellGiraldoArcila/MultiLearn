c48a6b8137e995ae8aa9cace4d514f6d95308fea
Michell Giraldo <michellarcila4861@gmail.com>
2026-04-25 12:34:07 -0500

    Cambio para frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || '';
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  'https://multilearn-backend.onrender.com';
 
 export const api = axios.create({
   baseURL,
